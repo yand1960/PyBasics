d223845611608cfc812ce605cf6b4e5ff63657c2
yand <yand@outlook.com>
2022-03-30 15:58:14 +0300

    YA

---
+++ py11_SQL.py
@@ -0,0 +1,34 @@
+import pymssql
+
+# PATTERN = "Silver"
+PATTERN = input("Введите буквы и нажмите ENTER: ")
+
+connection = pymssql.connect(
+    server="yand.dyndns.org",
+    database = "AdventureWorks",
+    user="northwind",
+    password="northwind"
+)
+
+sql = f"""
+    SELECT ProductID, Name,  ProductNumber, ListPrice
+    FROM AdventureWorks.Production.Product
+    WHERE Name LIKE '%{PATTERN}%'
+"""
+
+cursor = connection.cursor()
+cursor.execute(sql)
+results = cursor.fetchall()
+cursor.close()
+connection.close()
+
+print(f"{'id':5}\t{'name':35}\t{'code':10}\t{'price'}")
+
+# print(results)
+for product in results:
+    # print(product)
+    id = product[0]
+    name = product[1]
+    code = product[2]
+    price = product[3]
+    print(f"{id:5}\t{name:35}\t{code:10}\t{price}")
